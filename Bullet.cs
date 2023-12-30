ef0c222294f97e8009f8ffc46c3f1a46d09e36dc
TEAMkung1 <151872437+TEAMkung1@users.noreply.github.com>
2023-12-30 07:56:58 +0700

    Add files via upload

--- Bullet.cs
+++ Bullet.cs
@@ -5,9 +5,9 @@ public class Bullet : MonoBehaviour
 
     private Transform target;
 
-    public float speed = 50f;
+    public float speed = 70f;
 
-    public int damage = 100;
+    public int damage = 50;
 
     public float explosionRadius = 0f;
     public GameObject impactEffect;
@@ -32,17 +32,57 @@ public class Bullet : MonoBehaviour
 
         if (dir.magnitude <= distanceThisFrame)
         {
-
+            HitTarget();
             return;
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
+
+    }
+
+    void HitTarget()
+    {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 5f);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
+        Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Enemy e = enemy.GetComponent<Enemy>();
+
+        if (e != null)
+        {
+            e.TakeDamage(damage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
-
-    
-
-
-
-
-   
